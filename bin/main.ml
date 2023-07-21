8fc4e7daa7ade71570b4eed2c7d11efc54537557
Lucas Rezzaghi <lucasrezzaghi@Lucass-MacBook-Air.local>
2023-07-20 21:21:16 -0300

    clean screen, and get screen size

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,51 @@
 open Unix
 open Bytes
 
-
-let raw_mode =
+let raw_mode config =
   let terminal = tcgetattr stdin in
-  let echo_state = { terminal with c_echo = true; c_icanon = false; c_isig = false; c_ixon = false } in
+  let echo_state =
+    {
+      terminal with
+      c_echo = true;
+      c_icanon = config;
+      c_brkint = config;
+      c_inpck = config;
+      c_istrip = config;
+      c_isig = config;
+      c_opost = config;
+      c_ixon = config;
+      c_icrnl = config;
+      c_vmin = 0;
+      c_vtime = 1;
+    }
+  in
   tcsetattr stdin TCSAFLUSH echo_state
 
-let rec main_loop pos buf =
-  let _ = raw_mode in
+let rec main_loop buf pos =
   let _ = read stdin buf pos 1 in
-  if get buf pos = Char.chr 4 then
-    print_bytes buf
-  else main_loop (pos + 1) buf
+  match get buf pos with
+  (* Handle ctrl-c *)
+  | '\003' ->
+      let _ = write stdout (of_string "\x1b[2J") 0 4 in
+      let _ = write stdout (of_string "\x1b[H") 0 3 in
+      ()
+  | _ -> main_loop buf (pos + 1)
 
-let () = main_loop 0 (create 1024)
+let rec draw_rows n =
+  if n = 0 then ()
+  else
+    let _ = write stdout (of_string "~\r\n") 0 3 in
+    draw_rows (n - 1)
+
+let () =
+  raw_mode false;
+  (* before cli buffer*)
+  let _ = write stdout (of_string "\x1b[2J") 0 4 in
+  let _ = write stdout (of_string "\x1b[H") 0 3 in
+  let rows = match Terminal_size.get_rows () with 
+  | Some i -> i
+  | _ -> 0 in
+  draw_rows rows;
+  let _ = write stdout (of_string "\x1b[H") 0 3 in
+  let _ = main_loop (create 1024) 0 in
+  raw_mode true
